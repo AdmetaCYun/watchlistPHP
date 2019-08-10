27c46f12ac64bd7c5cabba6b4cadaf24ec22880f
Danilo Oliverai <DDanilo000@gmail.com>
2019-08-10 21:59:48 +0100

    Create models and migrations

---
+++ database/migrations/2019_08_10_203347_create_episodes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('anime_id')->unsigned();
+            $table->Integer('ep');
+            $table->timestamps();
+
+            $table->foreign('anime_id')->references('id')->on('animes')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
